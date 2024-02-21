fba299a1cfb27093bf4ec2065b48317c9da09a0b
Md Antonin Islam <antonin.islam.dev@gmail.com>
2024-02-21 15:07:30 +0600

    adds particles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
 import { Component } from '@angular/core';
+import {
+  MoveDirection,
+  OutMode,
+  ISourceOptions,
+  Container,
+  Engine
+} from '@tsparticles/engine';
+
+import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { NgParticlesService } from '@tsparticles/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
+  id = 'tsparticles';
+  particlesOptions: ISourceOptions = {
+    background: {
+      color: {
+        value: '#1d1f21',
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+        },
+        onHover: {
+          enable: true,
+        },
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: '#ffffff',
+      },
+      links: {
+        color: '#ffffff',
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: 'circle',
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  constructor(private readonly ngParticlesService: NgParticlesService) {}
+
+  ngOnInit(): void {
+    void this.ngParticlesService.init(async (engine: Engine) => {
+      console.log("init", engine);
+
+      await loadSlim(engine);
+    });
+  }
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxParticlesModule } from "@tsparticles/angular";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
