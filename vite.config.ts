adbe413000f54fa7a3a70f4107cf19a7fc9eb180
Md Antonin Islam <antonin.islam@gmail.com>
2024-07-04 00:40:49 +0600

    removes base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/antonin686.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
